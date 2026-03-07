06e58066e717ce3aa8f00b43201dea3424888a3b
diegomarinsek <marinseksamuel@gmail.com>
2026-03-08 00:41:01 +0100

    ok

--- EsercitazioneWebAPP/Biblioteca/query.php
+++ EsercitazioneWebAPP/Biblioteca/query.php
@@ -1,29 +1,29 @@
-<?php 
-$conn = mysqli_connect("localhost","root","","Biblioteca");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $conn = mysqli_connect("localhost","root","","Biblioteca");
 if (isset($_GET['eta'])){
 $eta = $_GET['eta'];
 $query = ("SELECT nome,cognome,eta FROM Utente WHERE eta = $eta");
 $result = mysqli_query($conn,$query);
-}
-
-
-
-
-
-
 
 if($result -> num_rows > 0){
     while($riga = $result -> fetch_assoc()){
-        echo "Nome: ".$riga->nome;
-        echo "Cognome: ".$riga->cognome;
-        echo "Eta: ".$riga->eta;
+        echo "Nome: ".$riga["nome"].<br>; 
+        echo "Cognome: ".$riga["cognome"].<br>; 
+        echo "Eta: ".$riga["eta"].<br>;
         
     }
 }
+}
 
-
-
-
-
-
-?>
+    
+    ?>
+</body>
+</html>
