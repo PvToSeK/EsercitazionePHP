195cc43d790c5a8abd43516137f3443440439a58
PvToSeK <marinseksamuel@gmail.com>
2026-03-18 13:13:21 +0100

    modifiche
    perfettamente funzionante

--- EsercitazioneWebAPP/Biblioteca/autentica.php
+++ EsercitazioneWebAPP/Biblioteca/autentica.php
@@ -4,14 +4,19 @@ $conn = mysqli_connect("localhost","root","","Biblioteca");
 if(isset($_POST["email"])&& isset($_POST["password"])){
 $email= $_POST["email"];
 $password = $_POST["password"];
-$query =("SELECT * FROM Utente Where email = ? AND psw = ?");
+$query =("SELECT id_utente FROM Utente Where email = ? AND psw = ?");
 if($stmt = $conn->prepare($query)){
     $stmt->bind_param("ss",$email,$password);
     $stmt->execute();
     $result= $stmt->get_result();
 }
-}
+    if($result -> num_rows > 0){
+        $riga = $result -> fetch_assoc();
+        $_SESSION["id_utente"] = $riga["id_utente"];
+        echo "<a href='mostra.php'>Vai a Mostra</a>";
 
+}
+}
 
 
 ?>
---
+++ EsercitazioneWebAPP/Biblioteca/mostra.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost","root","","Biblioteca");
+if(isset($_SESSION["id_utente"])){
+$id_utente = $_SESSION["id_utente"];
+$query =("SELECT * FROM Utente Where id_utente = ?");
+$stmt= $conn->prepare($query);
+$stmt->bind_param("i",$id_utente);
+$stmt->execute();
+$result= $stmt->get_result();
+    if($result -> num_rows > 0){
+        $riga = $result -> fetch_assoc();
+        echo "Nome: ".$riga["nome"]."<br>"; 
+        echo "Cognome: ".$riga["cognome"]."<br>"; 
+        echo "Eta: ".$riga["eta"]."<br>";
+}
+}
+?>
